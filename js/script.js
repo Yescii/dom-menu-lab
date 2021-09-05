def4d18344c8fb0831e502d407e261f49c516520
Yescii <cgonzalez.jesse@gmail.com>
2021-09-04 20:08:04 -0400

    no changes, just organized the code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
+/* MY NOTES
+
+ "ERROR is not a function", means it uses an equal sign
+
+*/
+
+
 const mainEl = document.querySelector('main');
-// is not a function means it uses an equal sign
 
-
+// added a background color to main element
 mainEl.style.backgroundColor = 'var(--main-bg)';
 
+// add h1 tag to main element with content
 const h1 = document.createElement('h1');
-h1.innerHTML = 'SEI Rocks!';
+h1.innerHTML = 'SEI Rocks!'; 
 mainEl.appendChild(h1);
+
+//add a class to main element
 mainEl.classList='flex-ctr';
 
