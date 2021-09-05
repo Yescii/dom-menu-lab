3f59714b3debe9a74ea5e0f18b57656f8bb4c03e
Yescii <cgonzalez.jesse@gmail.com>
2021-09-04 20:36:27 -0400

    added a height to the nav element with background color, and addded css class

--- js/script.js
+++ js/script.js
@@ -10,11 +10,19 @@ const mainEl = document.querySelector('main');
 // added a background color to main element
 mainEl.style.backgroundColor = 'var(--main-bg)';
 
-// add h1 tag to main element with content
+// added h1 tag to main element with content
 const h1 = document.createElement('h1');
 h1.innerHTML = 'SEI Rocks!'; 
 mainEl.appendChild(h1);
 
-//add a class to main element
+//added a class to main element
 mainEl.classList='flex-ctr';
+
+
+//added a height of 100% to the nav element with a background color, and added css class 
+const topMenuEl = document.getElementById("top-menu");
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+topMenuEl.classList= "flex-around";
+
 
